aa3b47511609c9af4a3eb33daf3b1327463b5934
Marvin Frommhold <depressiveRobot@users.noreply.github.com>
2017-04-12 14:15:31 +0200

    Use only normal ansi colors

--- pure-time.zsh
+++ pure-time.zsh
@@ -139,7 +139,7 @@ prompt_pure_preprompt_render() {
 	[[ -n ${prompt_pure_cmd_timestamp+x} && "$1" != "precmd" ]] && return
 
 	# set color for git branch/dirty status, change color if dirty checking has been delayed
-	local git_color=245
+	local git_color=fg
 	[[ -n ${prompt_pure_git_last_dirty_check_timestamp+x} ]] && git_color=red
 
 	# construct preprompt, beginning with path
@@ -152,7 +152,7 @@ prompt_pure_preprompt_render() {
 	preprompt+=$prompt_pure_username
 
 	# current time
-	local rpreprompt="%F{245}$(date +%H:%M:%S)%f"
+	local rpreprompt="%F{fg}$(date +%H:%M:%S)%f"
 
 	integer preprompt_left_length preprompt_right_length space_length
 	prompt_pure_string_length_to_var "${preprompt}" "preprompt_left_length"
@@ -376,10 +376,10 @@ prompt_pure_setup() {
 	fi
 
 	# show username@host if logged in through SSH
-	[[ "$SSH_CONNECTION" != '' ]] && prompt_pure_username=' %F{245}%n@%m%f'
+	[[ "$SSH_CONNECTION" != '' ]] && prompt_pure_username=' %F{fg}%n@%m%f'
 
-	# show username@host if root, with username in white
-	[[ $UID -eq 0 ]] && prompt_pure_username=' %F{white}%n%f%F{245}@%m%f'
+	# show username@host if root, with username in red
+	[[ $UID -eq 0 ]] && prompt_pure_username=' %F{red}%n%f%F{fg}@%m%f'
 
 	# prompt turns red if the previous command didn't exit with 0
 	PROMPT="%(?.%F{yellow}.%F{red})${PURE_PROMPT_SYMBOL:-❯}%f "
